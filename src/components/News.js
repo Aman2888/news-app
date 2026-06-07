c66bbf0c3c38404972f54892a69a4b16e89c0ace
Amna Irfan <amnai1899@gmail.com>
2026-06-07 22:31:22 +0500

    Switch to GNews API

--- src/components/News.js
+++ src/components/News.js
@@ -15,7 +15,7 @@ const News = (props) => {
 
   const updateNews = async () => {
     props.setProgress(10);
-    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=10926557114f4a8999cb0852b48ad92a&page=${page}&pageSize=${props.pageSize}`;
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=566e9ee5cb9a12ee4ad9f77e5c0ee8d1&page=${page}&pageSize=${props.pageSize}`;
     setLoading(true)
     let data = await fetch(url);
     props.setProgress(30);
@@ -28,7 +28,7 @@ const News = (props) => {
   }
   
   const fetchMoreData = async () => {
-    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=10926557114f4a8999cb0852b48ad92a&page=${page + 1}&pageSize=${props.pageSize}`;
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=566e9ee5cb9a12ee4ad9f77e5c0ee8d1&page=${page + 1}&pageSize=${props.pageSize}`;
     setPage(page + 1)
     
     let data = await fetch(url);
